e703da143d460c12233a93ef8fd7d5fd794d3339
Orlando Murcio <ato.murcio@gmail.com>
2023-05-16 19:30:32 -0600

    Create axios call

---
+++ src/utilities/fetch.ts
@@ -0,0 +1,27 @@
+// fetch.js
+import { ref, Ref } from 'vue'
+
+interface FetchResult<T> {
+  data: Ref<T | null>
+  error: Ref<Error | null>
+}
+
+export async function useFetch<T>(url: string): Promise<FetchResult<T>> {
+  const data: Ref<T | null> = ref(null)
+  const error: Ref<Error | null> = ref(null)
+
+  try {
+    const response = await fetch(url)
+
+    if (!response.ok) {
+      throw new Error(`Request failed with status ${response.status}`)
+    }
+
+    const json: T = await response.json()
+    data.value = json
+  } catch (err: unknown) {
+    error.value = err instanceof Error ? err : new Error(String(err))
+  }
+
+  return { data, error }
+}
